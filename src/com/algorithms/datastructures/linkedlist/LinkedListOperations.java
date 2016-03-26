d72606442122e983c724af8d9147fa0bb7806737
nsit_karan <nsit_karan@yahoo.com>
2016-03-26 02:31:30 -0700

    reverse a list (non-recursive solution using 2 pointers)

--- src/com/algorithms/datastructures/linkedlist/LinkedListOperations.java
+++ src/com/algorithms/datastructures/linkedlist/LinkedListOperations.java
@@ -8,14 +8,40 @@ public class LinkedListOperations {
 		this.list = list;
 	}
 
-
+	/**
+	 * Brief:
+	 * keep track of the list using 2 pointers(or references :)):
+	 * p and q :
+	 * 
+	 * |a|-->|b|-->|c|-->|d|
+	 * 
+	 * p points to a
+	 * q points to b.
+	 * 
+	 * At each step, 2 operations are done:
+	 * - b is made to point to a(i.e, b.next = a)
+	 * - a and b are both advanced by 1
+	 * 
+	 * So, in short, q is made to point to p
+	 *  
+	 */
 	public void reverse() {
-		/*
-		 * If only 1 element exists, no-op
-		 */
-		if (list.size() == 1) {
-			return;
+		
+		Node p = list.head;
+		Node q = p.next;
+		
+		list.head.next = null;
+		list.tail = p;
+		
+		while (q != null) {
+			Node t = q;
+			q = q.next;
+			t.next = p;
+			p = t;
 		}
+		
+		list.head = p;
+		
 	}
 	
 
@@ -34,6 +60,7 @@ public class LinkedListOperations {
 		listOperations.initializeList();
 		listOperations.list.displayList("initial list");
 		listOperations.reverse();
+		listOperations.list.displayList("reversed list");
 	}
 
 }
