67ed392914728337e80368dc9b44e59d367d52df
nsit_karan <nsit_karan@yahoo.com>
2016-03-28 08:09:08 -0700

    burn beighbour problem(unsolved)

---
+++ src/com/algorithms/misc/BurnNeighbourProblem.java
@@ -0,0 +1,35 @@
+package com.algorithms.misc;
+
+/**
+ * Problem:
+ * given a set of match sticks which are arranged in a rectangle
+ * (think of it as each entry in the matrix represents a match).
+ * 
+ * Now, match at (0,0) is lit up.
+ * Rules:
+ * - if a match is lit up, it takes 1 second for it to be lighted and
+ * after 1 second it can light up its neighbours(no diagnol).
+ * 
+ * Ask:
+ * - after how much time will all the matches be lit
+ * - complexity?
+ * 
+ * Solution:
+ * pending
+ * 
+ * @author dracula
+ *
+ */
+public class BurnNeighbourProblem {
+	
+	public int getTime(int matches[][]) {
+		return 0;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
