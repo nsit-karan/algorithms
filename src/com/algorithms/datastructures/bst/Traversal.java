fb63433e75c1a82c9aae079e3cd1948c5cc22831
nsit_karan <nsit_karan@yahoo.com>
2017-12-10 02:17:08 -0800

    logic to find in-order successor of a node

--- src/com/algorithms/datastructures/bst/Traversal.java
+++ src/com/algorithms/datastructures/bst/Traversal.java
@@ -13,6 +13,25 @@ public class Traversal {
 	}
 	
 	/*
+	 * Find the element which is 'just' larger than ptr.
+	 * Approach:
+	 * - go on going left down the subtree rooted at ptr.right
+	 * - stop when the node no longer has a left-child. That is the node which is just larger than ptr but only just :) 
+	 */
+	public TreeNode findInOrderSuccessor(TreeNode ptr) {
+
+		TreeNode current = ptr.right;
+		while (current != null) {
+			if (current.left == null) {
+				break;
+			}
+			current = current.left;
+		}
+		
+		return current;
+	}
+	
+	/*
 	 * complexity O(n) where the no of nodes is n
 	 */
 	public void inorderTreeWalk(TreeNode ptr) {
@@ -52,6 +71,7 @@ public class Traversal {
 		o.insertNode(tree, new TreeNode(30));
 		o.insertNode(tree, new TreeNode(5));
 		o.insertNode(tree, new TreeNode(200));
+		o.insertNode(tree, new TreeNode(175));
 		
 		Traversal t = new Traversal();
 		t.inorderTreeWalk(tree.head);
@@ -65,6 +85,14 @@ public class Traversal {
 		 * FindMax : 200
 		 */
 		System.out.println("max element is " + new Traversal().findMax(tree.head).data);
+
+		/*
+		 * Find in-order successor:
+		 * - for 100, will be 150
+		 * - for 150, will be 175
+		 */
+		System.out.println("in order successor of 100 is " + new Traversal().findInOrderSuccessor(tree.head).data);
+		System.out.println("in order successor of 150 is " + new Traversal().findInOrderSuccessor(tree.head.right).data);
 
 	}
 
