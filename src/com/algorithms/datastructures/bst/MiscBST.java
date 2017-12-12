6d9cd5e210bcf698ea95b48c758155871f7cb80f
nsit_karan <nsit_karan@yahoo.com>
2017-12-12 10:28:44 -0800

    misc bst operations

---
+++ src/com/algorithms/datastructures/bst/MiscBST.java
@@ -0,0 +1,18 @@
+package com.algorithms.datastructures.bst;
+
+public class MiscBST {
+
+	/*
+	 * verify whether the input tree is BST or not
+	 */
+	public boolean verifyBST() {
+		return false;
+	}
+	
+	/*
+	 * copy source to dest node by node
+	 */
+	public void copyBST(TreeNode source, TreeNode dest) {
+		
+	}
+}
